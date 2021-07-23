70eb2462567c98927ccee0abc2d4fdcb552c4fb3
Timur Zhdanov <bedarevos@gmail.com>
2021-07-23 17:19:18 +0300

    Homework_3

---
+++ Lesson_3/Homework_1_L3.py
@@ -0,0 +1,8 @@
+string1 = 'Съешь ещё этих мягких французских булок ДА выпей же чаю'
+word = string1.split()
+print(word[3].upper())
+print(word[6].lower())
+symbol = word[7]
+print(symbol[2])
+for i in word:
+    print (i)
