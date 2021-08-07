bf8018572db0e82cdecb78aefe16beed8a580d9f
Timur Zhdanov <bedarevos@gmail.com>
2021-08-07 12:06:31 +0300

    first

---
+++ Lesson_4/example_1.py
@@ -0,0 +1,8 @@
+# new_dict = {"dog:собака", "cat:кошка", "snake:змея"}
+# new_dict.items()
+# new_dict.values()
+
+
+key_list = ["a", "b", "c", "d"]
+dictionary = dict.fromkeys(key_list)
+print(dictionary)
